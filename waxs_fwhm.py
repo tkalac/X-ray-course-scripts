81fa03eaa4fd226632ff08350ea2a80d22f4ce3c
tkalac <47610781+tkalac@users.noreply.github.com>
2023-05-15 20:07:03 +0300

    Update waxs_fwhm.py
    - added baseline adjustment
    - style fixes (font, proportions, ...)

--- waxs_fwhm.py
+++ waxs_fwhm.py
@@ -9,13 +9,13 @@ from matplotlib import pyplot as plt
 import numpy as np
 from scipy import signal
 
-
 ########################
 #      DATA INPUT      #
 ########################
+
+### Data import
 data = np.genfromtxt("WAXS_AuNP.txt", dtype=float, names=True)
 data_bg = np.genfromtxt("WAXS_AuNP_background.txt", dtype=float, names=True)
-
 
 ### Background subctraction
 data["I"]=data["I"]-data_bg["I"]
@@ -24,10 +24,16 @@ data["I"]=data["I"]-data_bg["I"]
 I_fit = signal.savgol_filter(data["I"], 25, 2)
 peaks, _ = signal.find_peaks(I_fit, height=data["I"].max()*0.9)
 
-### Determining the FWHM
-FWHM_indicies=signal.peak_widths(I_fit, peaks, rel_height=0.5)
+### Baeline and FWHM
+baseline_height=0.86
+
+FWFM_indicies=signal.peak_widths(I_fit, peaks, rel_height=baseline_height)
+FWHM_indicies=signal.peak_widths(I_fit, peaks, rel_height=baseline_height/2)
 
 ### Conversion from indicies to values
+FWFM=FWFM_indicies[0]*(data["Q"][1]-data["Q"][0])
+FWFM=FWFM.astype(str)
+
 FWHM=FWHM_indicies[0]*(data["Q"][1]-data["Q"][0])
 FWHM=FWHM.astype(str)
 
@@ -43,28 +49,31 @@ print("FWHM:", FWHM)
 #        GRAPHS        #
 ########################
 
+##  STYLE
 plt.style.use('classic')
-plt.figure(figsize=(10,5))
+plt.rcParams["font.family"] = "Arial"
+plt.rcParams['font.size'] = 11
+plt.figure(figsize=(8,5))
 
-#   _____DATA_____
+### DATA
 plt.plot(data["Q"], data["I"], label="AuNP", color="black")
 
-#   ___ANNOTATIONS___
+### ANNOTATIONS
+#   Peak
 plt.plot(data["Q"][peaks], data["I"][peaks], "x", color="red", markersize=10)
+
+#   Baseline and FWHM line 
 plt.hlines(FWHM_indicies[1], data["Q"][FWHM_indicies[2].astype(int)], data["Q"][FWHM_indicies[3].astype(int)], color="red", linewidth=2)
+plt.hlines(FWFM_indicies[1], data["Q"][FWFM_indicies[2].astype(int)], data["Q"][FWFM_indicies[3].astype(int)], color="green", linewidth=2)
+
+#   Pointer and text
 plt.plot([data["Q"][FWHM_indicies[3].astype(int)], data["Q"][FWHM_indicies[3].astype(int)+50]], [FWHM_indicies[1], FWHM_indicies[1]*1.25], color="red", linewidth=0.5)
 plt.text(data["Q"][FWHM_indicies[3].astype(int)+50], FWHM_indicies[1]*1.25, "FWHM: %f"%FWHM, color="red")
-
-
-########################
-#     ANNOTATIONS      #
-########################
 
 plt.title("WAXS scattering intensity")
 plt.xlabel("q $[\AA ^{-1}]$")
 plt.ylabel("Intensity $[A.U.]$")
 plt.legend(loc="upper left")
 plt.grid(True)               
-
 
 plt.savefig("AuNP_graph.png", dpi=300)
